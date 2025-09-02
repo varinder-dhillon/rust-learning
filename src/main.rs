609c5e006289c381914e64c4294500d7e5179553
Varinder Singh <varinderdhilloninfo@gmail.com>
2025-09-02 23:52:23 +0530

    Learn (variables): Learned how to add variables and use in println

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main () {
-    println!("Hello world!")
+    let apples = 10;
+    let oranges = 30;
+
+    println!("I've total {apples} apples and {oranges} oranges. Total fruits {}.", apples+oranges)
+ 
+    // println!("Hello world!")
 }
