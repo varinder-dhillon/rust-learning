d2bb780013159ab71aeb45ad2a0531149f0f11f3
Varinder Singh <varinderdhilloninfo@gmail.com>
2025-09-04 23:15:32 +0530

    Positioned based arguments

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ fn main () {
     let apples = 10;
     let oranges = 30;
 
-    println!("I've total {apples} apples and {oranges} oranges. Total fruits {}.", apples+oranges)
+    // println!("I've total {apples} apples and {oranges} oranges. Total fruits {}.", apples+oranges)
+
+    println!("I've total {0} apples and {1} oranges. Total fruits {2}", apples, oranges, apples+oranges)
  
     // println!("Hello world!")
 }
